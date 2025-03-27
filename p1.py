558a3294e6df2a50abd2ffd06fd1f15d06d9ef7f
AndreaAl <andrealabandamunoz67@gmail.com>
2025-03-27 12:09:08 +0100

    Valido

--- p1.py
+++ p1.py
@@ -1,8 +1,5 @@
 print("Fichero python 1")
 print("Agrego nueva línea fichero python 1 desde VSC")
 print("Agrego nueva línea fichero python 1 desde VSC rama master")
-<<<<<<< HEAD
 print("Otra línea fichero python 1 desde VSC rama master")
-=======
 print("Nueva línea fichero python 1 desde GitHub.")
->>>>>>> 4e2df631f005e0d9d7d5d6e7199e0fb8cd7fb298
